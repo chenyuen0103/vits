54a0820709a9346622c44599a8cba31792493997
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-02-20 21:38:25 -0600

    change path

---
+++ datasets/__init__.py
@@ -0,0 +1,3 @@
+from celebA_dataset import celebADataset, celebAOodDataset, get_celebA_dataloader, get_celebA_dataset
+from waterbirds_dataset import WaterbirdDataset, get_waterbird_dataloader, get_waterbird_dataset
+from color_mnist import get_biased_mnist_dataloader, get_biased_mnist_dataset
