fix validation loss computation

train_bit.py
@@ -60,7 +60,7 @@ def run_eval(model, data_loader, device, chrono, logger, step):
   logger.flush()
   return np.mean(all_top1)
 
-def train_model(args):
+def train_model(seed,args):
   logger = bit_common.setup_logger(args)
   logger.info(f"Fine-tuning {args.model_type} on {args.dataset}")
   torch.backends.cudnn.benchmark = True

train_vit.py
@@ -102,7 +102,7 @@ def valid(args, model, writer, testset, test_loader, global_step):
                 eval_losses.update(eval_loss.item())
             else:
                 eval_loss = loss_fct(logits, y)
-                val_loss_computer.loss(logits, y)
+                val_loss_computer.loss(logits, y, env)
                 eval_losses.update(eval_loss.item())
 
             preds = torch.argmax(logits, dim=-1)