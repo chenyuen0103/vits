103cc6ce438ddab6c3febfdf69b1e691939fc842
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-02-23 21:43:15 -0600

    check path

--- launch_celeba_hessian.py
+++ launch_celeba_hessian.py
@@ -16,5 +16,8 @@ def main():
 
     for seed, grad_alpha, hess_beta in tqdm(itertools.product(seed_list, grad_alpha_values, hess_beta_values),
                                             desc='Evaluation'):
+        grad_alpha_formatted = "{:.1e}".format(grad_alpha).replace('.0e', 'e')
+        hess_beta_formatted = "{:.1e}".format(hess_beta).replace('.0e', 'e')
         eval_command = (f'python evaluate.py --name celeba_hessian --model_arch ViT --model_type ViT-S_16 --dataset celebA --batch_size 64 --img_size 384 '
-                        f'--checkpoint_dir output/waterbirds_hessian/waterbirds/ViT/ViT-S_16')
+                        f'--checkpoint_dir output/celeba_hessian/celebA/ViT/ViT-S_16/HessianERM/grad_alpha_{grad_alpha_formatted}_hess_beta_{hess_beta_formatted}/s{seed}/ViT-S_16')
+        os.system(eval_command)
