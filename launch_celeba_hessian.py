b376f9360632b34dbd94b9b61c3cba0392e01569
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-03-03 13:28:32 -0600

    skip experiments

--- launch_celeba_hessian.py
+++ launch_celeba_hessian.py
@@ -1,7 +1,7 @@
 import os
 import itertools
 from tqdm import tqdm
-
+import pandas as pd
 
 
 def main():
@@ -10,8 +10,18 @@ def main():
     # hess_beta_values = [0, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9][::-1]
     grad_alpha_values = [1, 1e-2, 1e-4,1e-6, 1e-8, 0]
     hess_beta_values = [1, 1e-2, 1e-4,1e-6, 1e-8, 0][::-1]
-    # maybe hess_beta_values = [1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9] to reduce the number of experiments
-    for seed, grad_alpha, hess_beta in tqdm(itertools.product(seed_list, grad_alpha_values, hess_beta_values), desc='CelebA Experiments'):
+    base_path = "./logs/celeba_hessian/celebA/ViT/ViT-S_16/HessianERM/"
+    for seed, grad_alpha, hess_beta in tqdm(itertools.product(seed_list, grad_alpha_values, hess_beta_values), desc='CUB Experiments'):
+        grad_alpha_formatted = "{:.1e}".format(grad_alpha).replace('.0e', 'e')
+        hess_beta_formatted = "{:.1e}".format(hess_beta).replace('.0e', 'e')
+
+        existing_path = os.path.join(base_path, f'grad_alpha_{grad_alpha_formatted}_hess_beta_{hess_beta_formatted}/s{seed}/train.csv')
+        existing_df = pd.read_csv(existing_path)
+
+        # Check the number of rows
+        if len(existing_df) >= 700:
+            print(f'Experiment with grad_alpha={grad_alpha} and hess_beta={hess_beta} already exists')
+            continue
         print(f'Running experiment with grad_alpha={grad_alpha} and hess_beta={hess_beta}')
         train_command = (f'python train.py --name celeba_hessian --model_arch ViT --model_type ViT-S_16 --dataset celebA --warmup_steps 100 '
                    f'--num_steps 700 --learning_rate 0.03 --batch_split 16 --img_size 384 --hessian_align --grad_alpha {grad_alpha} --hess_beta {hess_beta} '
--- launch_cub_hessian.py
+++ launch_cub_hessian.py
@@ -1,6 +1,7 @@
 import os
 import itertools
 from tqdm import tqdm
+import pandas as pd
 
 
 def main():
@@ -11,9 +12,19 @@ def main():
     hess_beta_values = [1, 1e-2, 1e-4, 1e-6, 1e-8, 0][::-1] #to reduce the number of experiments
     # grad_alpha_values = [1e-4]
     # hess_beta_values = [1e-4]
+    base_path = "./logs/waterbirds_hessian/waterbirds/ViT/ViT-S_16/HessianERM/"
     for seed, grad_alpha, hess_beta in tqdm(itertools.product(seed_list, grad_alpha_values, hess_beta_values), desc='CUB Experiments'):
-        # if grad_alpha == 1e-4 and hess_beta == 1e-4:
-        #     continue
+        grad_alpha_formatted = "{:.1e}".format(grad_alpha).replace('.0e', 'e')
+        hess_beta_formatted = "{:.1e}".format(hess_beta).replace('.0e', 'e')
+
+        existing_path = os.path.join(base_path, f'grad_alpha_{grad_alpha_formatted}_hess_beta_{hess_beta_formatted}/s{seed}/train.csv')
+        existing_df = pd.read_csv(existing_path)
+
+        # Check the number of rows
+        if len(existing_df) >= 700:
+            print(f'Experiment with grad_alpha={grad_alpha} and hess_beta={hess_beta} already exists')
+            continue
+
         print(f'Running experiment with grad_alpha={grad_alpha} and hess_beta={hess_beta}')
         train_command = (f'python train.py --name waterbirds_hessian --model_arch ViT --model_type ViT-S_16 --dataset waterbirds --warmup_steps 100 '
                    f'--num_steps 700 --learning_rate 0.03 --batch_split 16 --img_size 384 --hessian_align --grad_alpha {grad_alpha} --hess_beta {hess_beta} '
