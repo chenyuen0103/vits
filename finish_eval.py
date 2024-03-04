bf5fe86ff83d0df3273fb412cd2330ef66fb0e4c
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-03-04 10:46:42 -0600

    finish eval

--- finish_eval.py
+++ finish_eval.py
@@ -2,13 +2,13 @@ import os
 # import pandas as pd
 
 # Define the base path for the directories to search
-out_path = "./output/waterbirds_hessian/waterbirds/ViT/ViT-S_16/HessianERM/"
-results_path = "./results/waterbirds_hessian/waterbirds/ViT/ViT-S_16/HessianERM/"
+out_path = "./output/celeba_hessian/celebA/ViT/ViT-S_16/HessianERM/"
+results_path = "./results/celeba_hessian/celebA/ViT/ViT-S_16/HessianERM/"
 # List to hold directories with train.csv having less than 700 rows
 
 
 if __name__ == '__main__':
-    dataset = 'waterbirds'
+    dataset = 'celebA'
     # Loop through each subdirectory in the base path
     for dir in os.listdir(out_path):
         # Construct the path to the train.csv file
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         # Check if the train.csv file exists
         if not os.path.exists(result_file):
             eval_command = (
-                f'python evaluate.py --name {dataset}_hessian --model_arch ViT --model_type ViT-S_16 --dataset {dataset} --batch_size 64 --img_size 384 '
+                f'python evaluate.py --name celeba_hessian --model_arch ViT --model_type ViT-S_16 --dataset {dataset} --batch_size 64 --img_size 384 '
                 f'--checkpoint_dir {model_path}')
             print("running command", eval_command)
             os.system(eval_command)
