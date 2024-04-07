a854f34076cb29d57de1662eae6c818aac6c4d12
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-04-07 14:48:58 -0500

    finish eval

--- finish_eval.py
+++ finish_eval.py
@@ -12,16 +12,17 @@ def run_eval():
     dataset = 'celebA'
     # Loop through each subdirectory in the base path
     for dir in os.listdir(out_path):
+        for seed in list(range(0, 5)):
         # Construct the path to the train.csv file
-        model_path = os.path.join(out_path, dir, "s0", "ViT-S_16")
-        result_file = os.path.join(results_path, dir, "s0", "ViT-S_16", "test_accuracy.csv")
-        # Check if the train.csv file exists
-        if not os.path.exists(result_file):
-            eval_command = (
-                f'python evaluate.py --name celeba_hessian --model_arch ViT --model_type ViT-S_16 --dataset {dataset} --batch_size 64 --img_size 384 '
-                f'--checkpoint_dir {model_path}')
-            print("running command", eval_command)
-            os.system(eval_command)
+            model_path = os.path.join(out_path, dir, f"s{seed}", "ViT-S_16")
+            result_file = os.path.join(results_path, dir, "s0", "ViT-S_16", "test_accuracy.csv")
+            # Check if the train.csv file exists
+            if not os.path.exists(result_file):
+                eval_command = (
+                    f'python evaluate.py --name celeba_hessian --model_arch ViT --model_type ViT-S_16 --dataset {dataset} --batch_size 64 --img_size 384 '
+                    f'--checkpoint_dir {model_path}')
+                print("running command", eval_command)
+                os.system(eval_command)
 
 
 
