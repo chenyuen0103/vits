23aa194ff6b64d2ad2c16ae4320693666ff283c8
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-04-07 15:07:20 -0500

    finish hessian

--- launch_celeba_hessian.py
+++ launch_celeba_hessian.py
@@ -38,6 +38,7 @@ def erm():
 
 def hessian():
     seed_list = [1,2,3,4]
+    num_tries = 3
     grad_alpha_hess_beta_pairs = [
         (0.01, 0.01),
         (0.0001, 0.01),
@@ -55,20 +56,20 @@ def hessian():
                                               desc='CelebA Experiments'):
         grad_alpha_formatted = "{:.1e}".format(grad_alpha).replace('.0e', 'e')
         hess_beta_formatted = "{:.1e}".format(hess_beta).replace('.0e', 'e')
-
         existing_path = os.path.join(base_path, f'grad_alpha_{grad_alpha_formatted}_hess_beta_{hess_beta_formatted}/s{seed}/train.csv')
-        if os.path.exists(existing_path):
-            existing_df = pd.read_csv(existing_path)
-            # Check the number of rows
-            if len(existing_df) >= 700:
-                print(f'Experiment with grad_alpha={grad_alpha} and hess_beta={hess_beta} already exists')
-                continue
+        for _ in range(num_tries):
+            if os.path.exists(existing_path):
+                existing_df = pd.read_csv(existing_path)
+                # Check the number of rows
+                if len(existing_df) >= 700:
+                    print(f'Experiment with grad_alpha={grad_alpha} and hess_beta={hess_beta} already exists')
+                    continue
 
-        print(f'Running experiment with grad_alpha={grad_alpha} and hess_beta={hess_beta}')
-        train_command = (f'python train.py --name celeba_hessian --model_arch ViT --model_type ViT-S_16 --dataset celebA --warmup_steps 100 '
-                   f'--num_steps 700 --learning_rate 0.03 --batch_split 16 --img_size 384 --hessian_align --grad_alpha {grad_alpha} --hess_beta {hess_beta} '
-                   f'--seed {seed}')
-        os.system(train_command)
+            print(f'Running experiment with grad_alpha={grad_alpha} and hess_beta={hess_beta}')
+            train_command = (f'python train.py --name celeba_hessian --model_arch ViT --model_type ViT-S_16 --dataset celebA --warmup_steps 100 '
+                       f'--num_steps 700 --learning_rate 0.03 --batch_split 16 --img_size 384 --hessian_align --grad_alpha {grad_alpha} --hess_beta {hess_beta} '
+                       f'--seed {seed}')
+            os.system(train_command)
         grad_alpha_formatted = "{:.1e}".format(grad_alpha).replace('.0e', 'e')
         hess_beta_formatted = "{:.1e}".format(hess_beta).replace('.0e', 'e')
         eval_command = (f'python evaluate.py --name celeba_hessian --model_arch ViT --model_type ViT-S_16 --dataset celebA --batch_size 64 --img_size 384 '
